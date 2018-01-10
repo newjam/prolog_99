c2049bda8e3d77c4b4d8571b5bed1531e6e69bea
Newman <newjam02@ccm-2610-d2.evergreen.edu>
2018-01-09 19:21:28 -0800

    playing with prolog

--- prolog_99.pl
+++ prolog_99.pl
@@ -8,7 +8,9 @@ my_last(X, XS)     :- XS = [X|[]].
 element_at(X, [X|_] , K) :- K = 0 .
 element_at(X, [_|XS], J) :- element_at(X, XS, K), J is K + 1.
 
-% P04 Find the number of elements of a list.
+% P04 Find the number of elements of a list..
+
+
 
 my_length(N, [])     :- N = 0.
 my_length(M, [_|XS]) :- my_length(N, XS), M is N + 1.
@@ -29,4 +31,50 @@ my_reverse(XS, YS) :- my_back(FRONT, XS, FIRST),
 % P06 (*) Find out whether a list is a palindrome.
 
 palindrome(XS) :- my_reverse(XS, XS).
+
+% P26 Combinations.
+:- op(450, xfy, [∧]).
+:- op(475, xfy, [∨]).
+:- op(500, xfy, [∈]).
+:- op(550, xfy, [⊆]).
+:- op(600, xfy, [→]).
+
+X ∈ [X|_].
+X ∈ [_|Xs] :- X ∈ Xs.
+
+P ∨ _ :- P.
+_ ∨ Q :- Q.
+
+P ∧ Q :- P, Q.
+
+P → Q :- \+ (P ∧ (\+ Q)).
+
+A ⊆ B :- X ∈ A → X ∈ B.
+
+%implies(P, _) :- \+ P.
+%implies(P, Q) :- P, Q.
+
+%when(Q, P) :- implies(P, Q).
+
+
+
+% if A subset B then A subset _
+
+%[X|A] my_subset [X|B] :- A my_subset B, is_set(A), is_set(B).
+%[]    my_subset A     :- is_set(A).
+
+
+
+% A is a subset of B if X in B when X in A.
+
+comb2(C, X) :- length(C, 2),
+               C ⊆ X.
+
+
+
+% [a, b, c]
+
+% [a, b]
+% [a, c]
+% [b, c]
 
